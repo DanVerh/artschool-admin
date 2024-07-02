0b39c0de9a5102e8ac77f227182722619cce68b4
DanVerh <danverh29@gmail.com>
2024-07-02 23:46:14 +0300

    comments added

--- backend/migration/application/app.go
+++ backend/migration/application/app.go
@@ -8,11 +8,13 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
 )
 
+// Create application struct (class) with required for migration fields
 type App struct {
 	file string
 	dbUri string
 }
 
+// Construct for the App object
 func New() *App {
 	app := &App{
 		file: "file://migrations",
@@ -22,6 +24,7 @@ func New() *App {
 	return app
 }
 
+// Start the app by running the migration
 func (app *App) Start() error {
 	m, err := migrate.New(
     	app.file,
